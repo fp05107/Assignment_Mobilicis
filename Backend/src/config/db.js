2889476f1bc8ddd68b51076e3cf434a070bbf643
subham <andharalauri@gmail.com>
2023-04-25 14:39:36 +0530

    updated

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const MONGOURl = process.env.MONGOURL;
 module.exports = connect = async () => {
-    console.log(typeof(MONGOURl))
+   
   return await mongoose.connect(MONGOURl);
 };
