47120a494f42cf0050da09e9f005c1c8fa0cca81
subham <andharalauri@gmail.com>
2023-04-26 20:09:04 +0530

    added URL

--- FrontEnd/mobilicis_frontend/src/Components/api.js
+++ FrontEnd/mobilicis_frontend/src/Components/api.js
@@ -1,28 +1,38 @@
-import axios from "axios"
+import axios from "axios";
+const URL = "https://better-hem-dove.cyclic.app";
+export const getUsers = async (params = {}) => {
+  let res = await axios.get(
+    `${URL}/?page=${params.page}&limit=${params.limit}`
+  );
+  return res.data;
+};
+export const getUsersByIncomeAndCar = async (params = {}) => {
+  let res = await axios.get(
+    `${URL}/usersByIncomeAndCar/?page=${params.page}&limit=${params.limit}`
+  );
+  return res.data;
+};
 
-export const getUsers = async (params = {}) =>{
-    let res = await axios.get(`http://localhost:8080/?page=${params.page}&limit=${params.limit}`);
-    return res.data;
-}
-export const getUsersByIncomeAndCar = async (params = {}) =>{
-    let res = await axios.get(`http://localhost:8080/usersByIncomeAndCar/?page=${params.page}&limit=${params.limit}`);
-    return res.data;
-}
+export const getUsersByPhonePrice = async (params = {}) => {
+  let res = await axios.get(
+    `${URL}/usersByPhonePrice/?page=${params.page}&limit=${params.limit}`
+  );
+  return res.data;
+};
+export const getUsersByLastNameM = async (params = {}) => {
+  let res = await axios.get(
+    `${URL}/usersByLastNameM/?page=${params.page}&limit=${params.limit}`
+  );
+  return res.data;
+};
+export const getUsersByCar = async (params = {}) => {
+  let res = await axios.get(
+    `${URL}/usersByCar/?page=${params.page}&limit=${params.limit}`
+  );
+  return res.data;
+};
 
-export const getUsersByPhonePrice = async (params = {}) =>{
-    let res = await axios.get(`http://localhost:8080/usersByPhonePrice/?page=${params.page}&limit=${params.limit}`);
-    return res.data;
-}
-export const getUsersByLastNameM = async (params = {}) =>{
-    let res = await axios.get(`http://localhost:8080/usersByLastNameM/?page=${params.page}&limit=${params.limit}`);
-    return res.data;
-}
-export const getUsersByCar = async (params = {}) =>{
-    let res = await axios.get(`http://localhost:8080/usersByCar/?page=${params.page}&limit=${params.limit}`);
-    return res.data;
-}
-
-export const getTopCities = async () =>{
-    let res = await axios.get(`http://localhost:8080/topCities`);
-    return res.data;
-}
+export const getTopCities = async () => {
+  let res = await axios.get(`${URL}/topCities`);
+  return res.data;
+};
